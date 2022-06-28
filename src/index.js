621d955d49a09ae650bc8111fb7575d2de545167
KareemHakem <km01286486191@gmail.com>
2022-06-28 11:58:29 +0200

    edit style

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./App";
 
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
